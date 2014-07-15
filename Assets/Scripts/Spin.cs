9c320aaff8808ec0510bbfd071c81a4140d44a9c
abstractmachine <douglas@abstractmachine.net>
2014-07-15 12:35:37 +0200

    Added spinning cube

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+    public Vector3 speeds = new Vector3(1.0f, 1.0f, 1.0f);
+
+    void Update() {
+        transform.Rotate(speeds);
+    }
+    
+}
